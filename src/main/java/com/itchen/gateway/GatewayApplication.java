4db81b72ae02bed761ec568eec5fc3ba031856b7
BibiChen <yongbing923@gmail.com>
2020-02-11 20:53:27 +0800

    编写 Spring Cloud Gateway

---
+++ src/main/java/com/itchen/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.itchen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务网关 .
+ *
+ * @author BibiChen
+ * @version v1.0
+ * @since 2020-02-11
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
